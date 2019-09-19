710eaf52ed8738e2fcdbe0a166a8d19a13168da6
Hongbo Chen <hc50@iu.edu>
2019-09-19 19:52:42 -0400

    task2 done

--- 01_SMTP/src/py/smtp.py
+++ 01_SMTP/src/py/smtp.py
@@ -2,13 +2,33 @@
 
 import os
 import time
+import smtplib
 import argparse
 
 # Entry function for sending mail via SMTP.  The input arguments allow you to
-# contruct a well-formed email message and send it a specific server.
+# construct a well-formed email message and send it a specific server.
+
 def send_mail(server, faddr, taddr, msg):
     print (server, faddr, taddr, msg)
-    pass
+
+    host = server
+    port = 25
+    smtpObj = smtplib.SMTP(host, port)
+
+    msg = ("From: %s\r\nSubject: Ghost Msg\r\nTo: %s\r\n\r\n %s") % (faddr, taddr, msg)
+
+    smtpObj.set_debuglevel(3)
+
+    try:
+        smtpObj.sendmail(faddr, taddr, msg)
+    except Exception as e:
+        print(e)
+
+    smtpObj.quit()
+
+    #print(msg)
+
+
 
 def main():
     parser = argparse.ArgumentParser(description="SICE Network SMTP Client")
